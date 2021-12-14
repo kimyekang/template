cc6deb3421a3e822f78c6f4535aa0c4828a51e10
kimyekang <gongg10@naver.com>
2021-12-14 22:58:35 +0900

    Base 제작중,,

--- app/src/main/java/com/example/my__template/feature/MainActivity.kt
+++ app/src/main/java/com/example/my__template/feature/MainActivity.kt
@@ -2,19 +2,49 @@ package com.example.my__template.feature
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.my__template.R
 import com.example.my__template.common.BaseActivity
 import com.example.my__template.databinding.ActivityMainBinding
 import com.example.my__template.feature.main.home.HomeFragment
+import com.example.my__template.feature.main.mypage.MyPageFragment
+import com.example.my__template.feature.main.schedule.ScheduleFragment
 import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportFragmentManager.beginTransaction().replace(R.id.main_frm, HomeFragment()).commitAllowingStateLoss()
+        supportFragmentManager.beginTransaction().replace(R.id.main_frm, MyPageFragment()).commitAllowingStateLoss()
+        Log.d("dsadasdassda", "시작")
 
+        binding.mainBtmNav.setOnNavigationItemSelectedListener(
+            BottomNavigationView.OnNavigationItemSelectedListener { item ->
+                when (item.itemId) {
 
+                    R.id.page_myPage -> {
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.main_frm, MyPageFragment())
+                            .commitAllowingStateLoss()
+                        return@OnNavigationItemSelectedListener true
+                    }
+                    R.id.page_scheduler -> {
+                        Log.d("dsadasdassda", "여기")
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.main_frm, ScheduleFragment())
+                            .commitAllowingStateLoss()
+                        return@OnNavigationItemSelectedListener true
+                    }
+                    R.id.page_home -> {
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.main_frm, HomeFragment())
+                            .commitAllowingStateLoss()
+                        return@OnNavigationItemSelectedListener true
+                    }
+                }
+                false
+            })
     }
 }
---
+++ app/src/main/java/com/example/my__template/feature/main/mypage/MyPageFragment.kt
@@ -0,0 +1,20 @@
+package com.example.my__template.feature.main.mypage
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.my__template.R
+
+class MyPageFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_page, container, false)
+    }
+
+}
